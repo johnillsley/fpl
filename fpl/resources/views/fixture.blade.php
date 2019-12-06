def7b6298560c56a23c2cc47e756ee6a635396ee
John Illsley <jsi22@bath.ac.uk>
2019-12-06 12:34:45 +0000

    Moved root folder

--- fpl/resources/views/fixture.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Page Title')
-
-@section('content')
-    <h1>Fixture for {{ $fixture->id }}</h1>
-
-@endsection
--- fpl/resources/views/fixtures.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Page Title')
-
-@section('content')
-    <h1>Fixtures for {{$week->name}}</h1>
-    @include('subviews.fixtures', ['fixtures' => $fixtures])
-@endsection
